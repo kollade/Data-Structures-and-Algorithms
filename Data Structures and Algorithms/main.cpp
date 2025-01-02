16eb7ada857be966a9561504ea3335f6a1594ff2
Kolade Amlabu <ndakolade2015@gmail.com>
2025-01-02 14:46:05 -0700

    Initial Commit

---
+++ Data Structures and Algorithms/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Data Structures and Algorithms
+//
+//  Created by Kolade Amlabu on 2025-01-02.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
